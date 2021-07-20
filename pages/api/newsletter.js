88e52b92bf12b08f614128ee3acf86d2785aae70
leedoopal <39582981+leedoopal@users.noreply.github.com>
2021-07-20 21:48:18 +0900

    내부 api route에서 mongoDB 쿼리 실행

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
 
@@ -6,6 +8,14 @@ function handler(req, res) {
       res.status(422).json({ message: 'Invalid email address.' });
       return;
     }
+
+    const client = await MongoClient.connect(
+      'mongodb+srv://1233wsh:qqssuNPC5jWdDdaG@cluster0.esdqh.mongodb.net/newsletter?retryWrites=true&w=majority',
+    );
+    const db = client.db();
+    await db.collection('emails').insertOne({ email: userEmail });
+
+    await client.close();
 
     console.log(userEmail);
     res.status(201).json({ message: 'Signed up!' });
