d9092f45d99711bd6eec623024a7396ac813e344
leedoopal <39582981+leedoopal@users.noreply.github.com>
2021-07-21 22:33:11 +0900

    에러핸들링 추가

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,5 +1,18 @@
 import { MongoClient } from 'mongodb';
 
+async function connectDatabase() {
+  const client = await MongoClient.connect(
+    'mongodb+srv://1233wsh:qqssuNPC5jWdDdaG@cluster0.esdqh.mongodb.net/events?retryWrites=true&w=majority',
+  );
+
+  return client;
+}
+
+async function insertDocument(client, document) {
+  const db = client.db();
+  await db.collection('newsletter').insertOne(document);
+}
+
 async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
@@ -9,15 +22,23 @@ async function handler(req, res) {
       return;
     }
 
-    const client = await MongoClient.connect(
-      'mongodb+srv://1233wsh:qqssuNPC5jWdDdaG@cluster0.esdqh.mongodb.net/events?retryWrites=true&w=majority',
-    );
-    const db = client.db();
-    await db.collection('newsletter').insertOne({ email: userEmail });
+    let client;
 
-    await client.close();
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({ message: 'Connecting to the database failed!' });
+      return;
+    }
 
-    console.log(userEmail);
+    try {
+      await insertDocument(client, { email: userEmail });
+      await client.close();
+    } catch (error) {
+      res.status(500).json({ message: 'Inserting data failed!' });
+      return;
+    }
+
     res.status(201).json({ message: 'Signed up!' });
   }
 }
