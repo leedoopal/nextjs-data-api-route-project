a1250efeaf3d97a106b6879aa9c3c0c63a97043e
leedoopal <39582981+leedoopal@users.noreply.github.com>
2021-07-22 21:32:09 +0900

    컴포넌트 안에서 context data 사용

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,13 +1,25 @@
-import { Fragment } from 'react';
+import React, { Fragment, useContext } from 'react';
 
+import Notification from '../ui/notification';
+import NotificationContext from '../../store/notification-context';
 import MainHeader from './main-header';
 
 function Layout(props) {
+  const notificationCtx = useContext(NotificationContext);
+  const activeNotification = notificationCtx.notification;
+
   return (
-    <Fragment>
+    <>
       <MainHeader />
       <main>{props.children}</main>
-    </Fragment>
+      {activeNotification && (
+        <Notification
+          title={activeNotification.title}
+          message={activeNotification.message}
+          status={activeNotification.status}
+        />
+      )}
+    </>
   );
 }
 
