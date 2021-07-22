42e349c6c9e47a557e4572b84fc4425aa44b7766
leedoopal <39582981+leedoopal@users.noreply.github.com>
2021-07-22 21:28:00 +0900

    context state 추가

--- store/notification-context.js
+++ store/notification-context.js
@@ -1,14 +1,30 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
 
 const NotificationContext = createContext({
   notification: null,
-  showNotification() {},
+  showNotification(notificationData) {},
   hideNotification() {},
 });
 
 export function NotificationContextProvider(props) {
+  const [activeNotification, setActiveNotification] = useState();
+
+  function showNotificationHandler(notificationData) {
+    setActiveNotification(notificationData);
+  }
+
+  function hideNotificationHandler() {
+    setActiveNotification(null);
+  }
+
+  const context = {
+    notification: activeNotification,
+    showNotification: showNotificationHandler,
+    hideNotification: hideNotificationHandler,
+  };
+
   return (
-    <NotificationContext.Provider>
+    <NotificationContext.Provider value={context}>
       {props.children}
     </NotificationContext.Provider>
   );
